58ab07b0bbf6496f9a3c1945d601925ae05e52ba
Simonrazer <lpsimonrazer@gmail.com>
2018-01-12 22:09:28 +0100

    Update blender.py

--- blender.py
+++ blender.py
@@ -2,18 +2,21 @@ from bpy.types import Node
 from arm.logicnode.arm_nodes import *
 import arm.nodes_logic
 
-class Inverse(Node, ArmLogicTreeNode):
+class TestNode(Node, ArmLogicTreeNode):
     '''Test node'''
     bl_idname = 'LNTestNode'
-    bl_label = 'Inverse'
+    bl_label = 'Test'
     bl_icon = 'GAME'
 
     def init(self, context):
         self.inputs.new('ArmNodeSocketAction', 'In')
+        self.inputs.new('NodeSocketShader', 'Value')
+        self.inputs.new('NodeSocketShader', 'Value')
+        self.inputs.new('NodeSocketShader', 'Value')
         self.outputs.new('ArmNodeSocketAction', 'Out')
 
 def register():
     # Add custom nodes
-    add_node(Inverse, category='Action')
+    add_node(TestNode, category='Variable')
     # Register newly added nodes
     arm.nodes_logic.register_nodes()
