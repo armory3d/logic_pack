08c6f91b0d74e6e14f7119ab5a1919234f06691a
Simonrazer <lpsimonrazer@gmail.com>
2018-01-13 16:15:19 +0100

    Update blender.py

--- blender.py
+++ blender.py
@@ -2,21 +2,18 @@ from bpy.types import Node
 from arm.logicnode.arm_nodes import *
 import arm.nodes_logic
 
-class TestNode(Node, ArmLogicTreeNode):
-    '''Test node'''
-    bl_idname = 'LNTestNode'
-    bl_label = 'Test'
+class ToBool(Node, ArmLogicTreeNode):
+    '''To Bool'''
+    bl_idname = 'LNToBool'
+    bl_label = 'ToBool'
     bl_icon = 'GAME'
 
     def init(self, context):
         self.inputs.new('ArmNodeSocketAction', 'In')
-        self.inputs.new('NodeSocketShader', 'Value')
-        self.inputs.new('NodeSocketShader', 'Value')
-        self.inputs.new('NodeSocketShader', 'Value')
-        self.outputs.new('ArmNodeSocketAction', 'Out')
-
+        self.outputs.new('NodeSocketBool', 'Bool')
+        
 def register():
     # Add custom nodes
-    add_node(TestNode, category='Variable')
+    add_node(ToBool, category='Logic') 
     # Register newly added nodes
     arm.nodes_logic.register_nodes()
