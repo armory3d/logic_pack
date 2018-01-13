233e47826c4bc95549c60dccc3feb1d6ce24e174
Simonrazer <lpsimonrazer@gmail.com>
2018-01-13 16:19:08 +0100

    all nodes

--- blender.py
+++ blender.py
@@ -11,9 +11,30 @@ class ToBool(Node, ArmLogicTreeNode):
     def init(self, context):
         self.inputs.new('ArmNodeSocketAction', 'In')
         self.outputs.new('NodeSocketBool', 'Bool')
-        
+
+class minmaxnode(Node, ArmLogicTreeNode):
+    '''Min/Max Node'''
+    bl_idname = 'LNminmaxnode'
+    bl_label = 'minmaxnode'
+    bl_icon = 'GAME'
+
+    def init(self, context):
+        self.inputs.new('ArmNodeSocketAction', 'In')
+
+class InverseNode(Node, ArmLogicTreeNode):
+    '''Inverse node'''
+    bl_idname = 'LNInverseNode'
+    bl_label = 'Inverse'
+    bl_icon = 'GAME'
+
+    def init(self, context):
+        self.inputs.new('ArmNodeSocketAction', 'In')
+self.outputs.new('ArmNodeSocketAction', 'Out')
+
 def register():
     # Add custom nodes
-    add_node(ToBool, category='Logic') 
+    add_node(ToBool, category='Logic')
+    add_node(minmaxnode, category='Variable')
+    add_node(InverseNode, category='Logic')
     # Register newly added nodes
     arm.nodes_logic.register_nodes()
