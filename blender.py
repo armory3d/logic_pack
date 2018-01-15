4e549cf107e4c2caee4b4108f2f536252138adce
Didiercode <didjich@gmail.com>
2018-01-15 18:54:33 +0100

    Update blender.py
    explained in the Forum at http://forums.armory3d.org/t/a-new-array-loop-node-with-indice-can-help-for-test-and-more/672

--- blender.py
+++ blender.py
@@ -1,7 +1,28 @@
-from bpy.types import Node
+import bpy
+from bpy.props import *
+
+
+from bpy.types import Node, NodeSocket
 from arm.logicnode.arm_nodes import *
 import arm.nodes_logic
 
+
+class ArrayLoopIndiceNode(Node, ArmLogicTreeNode):
+    '''ArrayLoop node avec indice'''
+    bl_idname = 'LNArrayLoopIndiceNode'
+    bl_label = 'Array Loop Indice'
+    bl_icon = 'CURVE_PATH'
+    
+    def init(self, context):
+        self.inputs.new('ArmNodeSocketAction', 'In')
+        self.inputs.new('NodeSocketShader', 'Array')
+        self.outputs.new('ArmNodeSocketAction', 'Loop')
+        self.outputs.new('NodeSocketInt', 'Value')
+        self.outputs.new('ArmNodeSocketAction', 'Done')
+        self.outputs.new('NodeSocketInt', 'Indice')
+
+
+        
 class ToBoolNode(Node, ArmLogicTreeNode):
     '''To Bool Node'''
     bl_idname = 'LNToBoolNode'
@@ -37,6 +58,7 @@ class InverseNode(Node, ArmLogicTreeNode):
 
 def register():
     # Add custom nodes
+    add_node(ArrayLoopIndiceNode, category='Logic')
     add_node(ToBoolNode, category='Logic')
     add_node(MinMaxNode, category='Variable')
     add_node(InverseNode, category='Logic')
