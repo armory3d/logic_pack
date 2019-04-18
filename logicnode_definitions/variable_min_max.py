6f3071865e556f4594baec802260d9354d001d3d
Simonrazer <lpsimonrazer@gmail.com>
2019-04-18 08:41:07 +0200

    Renaming of MinMax node (Clamp Variable)

--- logicnode_definitions/variable_min_max.py
+++ logicnode_definitions/variable_min_max.py
@@ -6,14 +6,14 @@ from arm.logicnode.arm_nodes import *
 class MinMaxNode(Node, ArmLogicTreeNode):
     '''Min/Max Node'''
     bl_idname = 'LNMinMaxNode'
-    bl_label = 'MinMax'
+    bl_label = 'Clamp Variable'
     bl_icon = 'QUESTION'
 
     def init(self, context):
         self.inputs.new('ArmNodeSocketAction', 'In')
         self.inputs.new('NodeSocketShader', 'Value')
-        self.inputs.new('NodeSocketShader', 'Value')
-        self.inputs.new('NodeSocketShader', 'Value')
+        self.inputs.new('NodeSocketFloat', 'Min')
+        self.inputs.new('NodeSocketFloat', 'Max')
         self.outputs.new('ArmNodeSocketAction', 'Out')
         
 add_node(MinMaxNode, category='Variable')
